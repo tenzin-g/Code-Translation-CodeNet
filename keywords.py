a04a0948ce25cc96d89ca8807a0992e7784a642c
tenzin-g <84874177+tenzin-g@users.noreply.github.com>
2022-10-10 22:18:25 -0400

    python and c++ keyword identification

--- keywords.py
+++ keywords.py
@@ -2,32 +2,126 @@
 import keyword
 
 #returns full list of python keywords
-def keywords():
+def pythonKeywords():
     list = keyword.kwlist
-    #print("No. of keywords present in current version :",
-    #len(list))
     return list
+#returns full list of C++ keywords
+def cpluspluskeywords():
+    keys = ['alignas',
+    'alignof',
+    'and',
+    'and_eq',
+    'asm',
+    'atomic_cancel',
+    'atomic_commit',
+    'atomic_noexcept',
+    'auto',
+    'bitand',
+    'bitor',
+    'bool',
+    'break',
+    'case',
+    'catch',
+    'char',
+    'char8_t',
+    'char16_t',
+    'char32_t',
+    'class',
+    'compl',
+    'concept',
+    'const',
+    'consteval',
+    'constexpr',
+    'constinit',
+    'const_cast',
+    'continue',
+    'co_await',
+    'co_return',
+    'co_yield',
+    'decltype',
+    'default',
+    'delete',
+    'do',
+    'double',
+    'dynamic_cast',
+    'else',
+    'enum',
+    'explicit',
+    'export',
+    'extern',
+    'false',
+    'float',
+    'for',
+    'friend',
+    'goto',
+    'if',
+    'inline',
+    'int',
+    'long',
+    'mutable',
+    'namespace',
+    'new',
+    'noexcept',
+    'not',
+    'not_eq',
+    'nullptr',
+    'operator',
+    'or',
+    'or_eq',
+    'private',
+    'protected',
+    'public',
+    'reflexpr',
+    'register',
+    'reinterpret_cast',
+    'requires',
+    'return',
+    'short',
+    'signed',
+    'sizeof',
+    'static',
+    'static_assert',
+    'static_cast',
+    'struct',
+    'switch',
+    'synchronized',
+    'template',
+    'this',
+    'thread_local',
+    'throw',
+    'true',
+    'try',
+    'typedef',
+    'typeid',
+    'typename',
+    'union',
+    'unsigned',
+    'using',
+    'virtual',
+    'void',
+    'volatile',
+    'wchar_t',
+    'while',
+    'xor',
+    'xor_eq']
+    return keys
 
 #reading python textfile and identifying its keywords
 #returns list of python keywords in file
-def identifyKeys(txtfile):
-    list = keyword.kwlist
-    print(list)
-    '''with open(txtfile) as f:
-        contents = f.read()
-        print(contents)'''
-    #f = open(textfile, 'r')
+def identifyKeys(txtfile, lang):
+    if lang == "python" or lang == "Python":
+        list = pythonKeywords()
+    elif lang == "c++" or lang == "C++":
+        list = cpluspluskeywords()
     output = []
     with open(txtfile) as f:
         for line in f:
             curr = []
             curr.append(line.strip())
-            #print(curr)
             for key in list:
                 if any(key in word for word in curr):
                     output.append(key)
-            #print(curr)
-            #print(line.strip())
     return set(output)
 if __name__ == '__main__':
-    print(identifyKeys('pythonKeywordTest.txt'))
+    print(identifyKeys('pythonKeywordTest.txt', "python"))
+    print(identifyKeys('c++KeywordTest.txt','c++' ))
