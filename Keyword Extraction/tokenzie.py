6d27234284048583783b59d162e996d0ec0a64e5
tenzin-g <84874177+tenzin-g@users.noreply.github.com>
2023-03-17 16:58:33 -0400

    Update tokenzie.py

--- Keyword Extraction/tokenzie.py
+++ Keyword Extraction/tokenzie.py
@@ -4,7 +4,19 @@
 #now ready to run!
 
 import nltk
+import csv
 #switch out with whatever txt file you have
 file_content = open("pythonKeywordTest.txt").read()
 tokens = nltk.word_tokenize(file_content)
 print (tokens)
+
+# data to be written row-wise in csv file
+data = tokens
+
+# opening the csv file in 'w+' mode
+file = open('test.csv', 'w+', newline ='')
+
+# writing the data into the file
+with file:
+    write = csv.writer(file)
+    write.writerows(data)
